5005b14f25a854c3109db19ad2dd6a8a187b3cc0
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-06-19 22:14:57 +0700

    Add wrap_quote()

--- R/usethis.R
+++ R/usethis.R
@@ -1,3 +1,8 @@
+
+# Mirror Args -------------------------------------------------------------
+
+
+
 #' Mirror function arguments
 #' @description Mirror names of ... to Rt side. Use to write function argument faster
 #' @param ... Arguments that has `Name = Value` pair
@@ -16,4 +21,39 @@ mirror_args <- function(..., .new_line = F) {
     purrr::map_chr(~paste0(.x , " = ", .x)) %>%
     purrr::reduce(paste, sep = ifelse(.new_line, ",\n", ", ")) %>%
     cat()
+}
+
+
+# Wrap Quote --------------------------------------------------------------
+
+
+#' Wrap Quote to Text
+#'
+#' Useful for turning texts to quoted variable names. The output will ignore any white spaces,
+#' new lines, or any punctuation (except underscore and dot).
+#'
+#' @param .chr Character vector containing texts to wrap
+#'
+#' @return A message to copy
+#' @export
+#'
+#' @examples wrap_quote("var_1, !var.2,  ,  `var3`")
+wrap_quote <- function(.chr) {
+
+  quoting <- c("\"")
+  # Split at White space or Newline
+  chr_splited <- .chr %>% stringr::str_split("[:space:]+")
+
+  # Extract Letters, Number, underscore or dot
+  chr_extracted <- chr_splited %>%
+    purrr::map(~stringr::str_extract(.x, "([:alnum:]|_|\\.)+")) %>%
+    purrr::map(na.omit)
+
+  chr_extracted %>%
+    # Surround Quote
+    purrr::map(~purrr::map_chr(.x, ~paste0(quoting, .x , quoting))) %>%
+    # Reduce by comma
+    purrr::map(~purrr::reduce(.x, paste, sep = ", ")) %>%
+    print_messages()
+
 }
