a943b989c74f93173345a524d6251e2d017f7ce0
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-06-19 22:15:17 +0700

    add(), times()

---
+++ R/rdname.R
@@ -0,0 +1,12 @@
+#' Basic arithmetic
+#'
+#' @param x,y numeric vectors.
+#' @name arith
+NULL
+#> NULL
+
+#' @rdname arith
+add <- function(x, y) x + y
+
+#' @rdname arith
+times <- function(x, y) x * y
