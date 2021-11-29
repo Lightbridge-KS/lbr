c56ceb68cac6fceed45e2d8a29770126914ee2e6
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-11-29 22:28:45 +0700

    Add `nomatch` arg to `encoder()`

--- R/transform.R
+++ R/transform.R
@@ -1,25 +1,45 @@
 #' Encode Vector
+#'
 #' @description Match and Encode vector
 #' @param x vector: input data to be matched.
 #' @param match vector: matching value to `x`
 #' @param encode vector: encoding vector same length as `match`
+#' @param nomatch Character: Indicate result if elements in `x` was not matched with `match`.
+#' It must be one of:
+#' * \strong{"NA"}: return `NA`
+#' * \strong{"original"}: return original elements in `x`
 #'
 #' @return encoded vector
-#' @details Elements that not match will return `NA`.
+#' @details Elements that not match will return values according to `nomatch`.
 #' @export
 #'
 #' @examples
 #' encoder(c("a","b","d"), c("a","b","c"), c("A","B","C"))
+#' encoder(c("a","b","d"), c("a","b","c"), c("A","B","C"), nomatch = "original")
 encoder <- function(x, # Any vector
                     match,
-                    encode = match # Encode that pair with match
+                    encode = match, # Encode that pair with match
+                    nomatch = c("NA", "original")
 ) {
 
+  nomatch <- match.arg(nomatch)
   if(length(match) != length(encode)) stop("`match` and `encode` must have same length", call. = F)
 
-  df <- data.frame(match = match, encode = encode)
-  index <- match(x, df$match)
+  index <- match(x, match)
+  encoded_may_NA <- encode[index]
 
-  df$encode[index]
-
+  # Control no matching cases
+  out <- switch (nomatch,
+                 # Leaves as `NA`
+                 "NA" = { encoded_may_NA },
+                 # Use Original `x`
+                 "original" = {
+                   encoded_may_NA[is.na(encoded_may_NA)] <- x[is.na(encoded_may_NA)]
+                   encoded <- encoded_may_NA
+                   encoded
+                 }
+  )
+  out
 }
+
+
